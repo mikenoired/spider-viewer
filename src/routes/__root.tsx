5c5154af0443ba8c623a2c6543038aa1e92d8c0c
Mike Noired <unkstd@mail.ru>
2026-03-22 16:52:49 +0300

    fix mobile

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -28,7 +28,8 @@ export const Route = createRootRouteWithContext<RouterContext>()({
 			},
 			{
 				name: "viewport",
-				content: "width=device-width, initial-scale=1",
+				content:
+					"width=device-width, initial-scale=1, maximum-scale=5, user-scalable=yes, viewport-fit=cover",
 			},
 			{
 				title: PROJECT_NAME,
