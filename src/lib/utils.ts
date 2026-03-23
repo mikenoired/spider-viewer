a6e8b7acaa4c0f18bc35ffdb070849be27bff3c0
Mike Noired <unkstd@mail.ru>
2026-03-24 01:10:09 +0300

    fix

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -6,34 +6,34 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 const enToRuMap = new Map([
-  ["A", "А"],
-  ["a", "а"],
-  ["B", "В"],
-  ["b", "ь"],
-  ["C", "С"],
-  ["c", "с"],
-  ["E", "Е"],
-  ["e", "е"],
-  ["K", "К"],
-  ["k", "к"],
-  ["M", "М"],
-  ["m", "м"],
-  ["H", "Н"],
-  ["h", "н"],
-  ["O", "О"],
-  ["o", "о"],
-  ["P", "Р"],
-  ["p", "р"],
-  ["T", "Т"],
-  ["t", "т"],
-  ["Y", "У"],
-  ["y", "у"],
-  ["X", "Х"],
-  ["x", "х"],
+	["A", "А"],
+	["a", "а"],
+	["B", "В"],
+	["b", "ь"],
+	["C", "С"],
+	["c", "с"],
+	["E", "Е"],
+	["e", "е"],
+	["K", "К"],
+	["k", "к"],
+	["M", "М"],
+	["m", "м"],
+	["H", "Н"],
+	["h", "н"],
+	["O", "О"],
+	["o", "о"],
+	["P", "Р"],
+	["p", "р"],
+	["T", "Т"],
+	["t", "т"],
+	["Y", "У"],
+	["y", "у"],
+	["X", "Х"],
+	["x", "х"],
 ])
 
 export const enToRuVisual = (text: string) =>
-  text.replace(/./g, char => enToRuMap.get(char) ?? char)
+	text.replace(/./g, char => enToRuMap.get(char) ?? char)
 
 export async function downloadResponseFile(response: Response, fileName: string) {
 	const blob = await response.blob()
