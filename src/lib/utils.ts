a52fd6f439265c508724a2a83c407b9bd4fec54a
Mike Noired <unkstd@mail.ru>
2026-03-24 01:08:12 +0300

    fix

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,39 +5,35 @@ export function cn(...inputs: ClassValue[]) {
 	return twMerge(clsx(inputs))
 }
 
-const enToRuMap: Record<string, string> = {
-	A: "А",
-	a: "а",
-	B: "В",
-	b: "ь",
-	C: "С",
-	c: "с",
-	E: "Е",
-	e: "е",
-	K: "К",
-	k: "к",
-	M: "М",
-	m: "м",
-	H: "Н",
-	h: "н",
-	O: "О",
-	o: "о",
-	P: "Р",
-	p: "р",
-	T: "Т",
-	t: "т",
-	Y: "У",
-	y: "у",
-	X: "Х",
-	x: "х",
-}
+const enToRuMap = new Map([
+  ["A", "А"],
+  ["a", "а"],
+  ["B", "В"],
+  ["b", "ь"],
+  ["C", "С"],
+  ["c", "с"],
+  ["E", "Е"],
+  ["e", "е"],
+  ["K", "К"],
+  ["k", "к"],
+  ["M", "М"],
+  ["m", "м"],
+  ["H", "Н"],
+  ["h", "н"],
+  ["O", "О"],
+  ["o", "о"],
+  ["P", "Р"],
+  ["p", "р"],
+  ["T", "Т"],
+  ["t", "т"],
+  ["Y", "У"],
+  ["y", "у"],
+  ["X", "Х"],
+  ["x", "х"],
+])
 
-export function enToRuVisual(text: string) {
-	return text
-		.split("")
-		.map(char => enToRuMap[char] ?? char)
-		.join("")
-}
+export const enToRuVisual = (text: string) =>
+  text.replace(/./g, char => enToRuMap.get(char) ?? char)
 
 export async function downloadResponseFile(response: Response, fileName: string) {
 	const blob = await response.blob()
